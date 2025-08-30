087d22d014db347a83e4da1e9e7b97a19c6828ce
ut4k <aquarium011@gmail.com>
2025-08-30 15:18:19 +0900

    柔軟に終了できるwebサーバー+テスト

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ut4k/go_todo_app
 
 go 1.24.4
+
+require golang.org/x/sync v0.16.0 // indirect
--- main_test.go
+++ main_test.go
@@ -1,7 +1,52 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
 
-func TestMainFunc(t *testing.T) {
-	go main()
+	"golang.org/x/sync/errgroup"
+)
+
+// func TestMainFunc(t *testing.T) {
+// 	go main()
+// }
+
+func TestRun(t *testing.T) {
+	// キャンセル可能なコンテキストを作成。
+	ctx, cancel := context.WithCancel(context.Background())
+	// errgroup でゴルーチンをまとめて管理
+	// 別ゴルーチンで run(ctx) を呼ぶ  HTTP サーバーをバックグラウンドで起動
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return run(ctx)
+	})
+	// サーバーにリクエスト
+	in := "message"
+	rsp, err := http.Get("http://localhost:18080/" + in)
+	if err != nil {
+		t.Errorf("failed to get: %+v", err)
+	}
+	// 終了時に自動クローズ(リソースリーク防止)
+	defer rsp.Body.Close()
+	// レスポンスボディ読み込み
+	got, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+
+	// 戻り値を検証する
+	want := fmt.Sprintf("Hello, %s!", in)
+	// want := fmt.Sprintf("aaaaaaaHello, %s!", in) //失敗確認用
+	if string(got) != want {
+		t.Errorf("want %q, but got %q", want, got)
+	}
+	// コンテキストをキャンセル → サーバーを停止
+	cancel()
+	// errgroup がゴルーチンの終了を待つ
+	if err := eg.Wait(); err != nil {
+		t.Fatal(err)
+	}
 }
